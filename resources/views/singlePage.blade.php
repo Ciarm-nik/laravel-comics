870e81332e4640d23374514c552efe2aa8da1ff3
Ciarm-nik <nikcia@libero.it>
2021-06-21 16:38:04 +0200

    Dati card importati

--- resources/views/singlePage.blade.php
+++ resources/views/singlePage.blade.php
@@ -9,7 +9,34 @@
 <div class="container">
 
 
-<h1>{{$cards["title"]}}</h1>
+    <h1>{{$cards["title"]}}</h1>
+    <h4>{{$cards["price"]}}</h4>
+    <p>{{$cards["description"]}}</p>
+
+
+    <h5>Art by:</h5>
+    <p> @foreach ($cards['artists'] as $artist)
+        <a href="#">{{ $artist }}</a>
+        @endforeach
+    </p>
+
+
+    <h5>Written by:</h5>
+    <p>@foreach ($cards['writers'] as $writer)
+        <a href="#">{{ $writer }}</a>
+        @endforeach
+    </p>
+
+    <p>{{$cards["series"]}}</p>
+    <p>{{$cards["price"]}}</p>
+    <p>{{$cards["sale_date"]}}</p>
+</div>
+
+
+
+
+
+
 
 </div>
 
