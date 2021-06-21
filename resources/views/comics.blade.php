534d8abbd152bc0c7624adc7359f76798141c35f
Ciarm-nik <nikcia@libero.it>
2021-06-21 18:20:22 +0200

    Aggiunto css e section info

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -10,12 +10,12 @@
 
         {{-- @dump($listaComics) --}}
         @foreach($listaComics as $key=> $com)
-        
+
         <div class="card">
-            
+
             <a href="{{route('pagina-singola-card', ['index' => $key])}}">
-            <img src="{{$com['thumb']}}" alt="">
-            <h4 class="uppercase">{{$com['title']}}</h4>   
+                <img src="{{$com['thumb']}}" alt="">
+                <h4 class="uppercase">{{$com['title']}}</h4>
             </a>
         </div>
         @endforeach
@@ -27,6 +27,50 @@
         </div>
 
     </div>
+
+</section>
+
+<section class="info_section">
+    <div class="container flex">
+        <div class="menu flex col-2">
+            <div class="infoMenu flex">
+                <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
+                <h4 class="uppercase">DIGITAL COMICS</h4>
+            </div>
+        </div>
+
+        <div class="menu flex col-2">
+            <div class="infoMenu flex">
+                <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+                <h4 class="uppercase">DC Merchandise</h4>
+            </div>
+        </div>
+
+         <div class="menu flex col-2">
+            <div class="infoMenu flex">
+                <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
+                <h4 class="uppercase">Subscription</h4>
+            </div>
+        </div>
+
+         <div class="menu flex col-2">
+            <div class="infoMenu flex">
+                <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
+                <h4 class="uppercase">Comic shop locator</h4>
+            </div>
+        </div>
+
+         <div class="menu flex col-2">
+            <div class="infoMenu flex">
+                <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="">
+                <h4 class="uppercase">Dc Power visa</h4>
+            </div>
+        </div>
+    </div>
+
+    
+
+
 
 </section>
 
--- resources/views/singlePage.blade.php
+++ resources/views/singlePage.blade.php
@@ -6,30 +6,79 @@
 
 {{-- <h1>dettaglio card</h1> --}}
 
-<div class="container">
+<div class="containerSingle">
+
+    <section class="top flex">
+
+        <div class="contentLeft col-8">
+            <h1 class="uppercase">{{$cards["title"]}}</h1>
+            <h4>{{$cards["price"]}}</h4>
+            <p>{{$cards["description"]}}</p>
+        </div>
+
+        <div class="contentRight col-4">
+            <img src="{{asset('images/adv.jpg')}}" alt="">
+        </div>
+    </section>
 
 
-    <h1>{{$cards["title"]}}</h1>
-    <h4>{{$cards["price"]}}</h4>
-    <p>{{$cards["description"]}}</p>
+
+    <section class="bottom">
+
+        <div class="row flex">
+            <div class="col-6">
+                <h3 class="uppercase">Talent</h3>
+            </div>
+            <div class="col-6">
+                <h3 class="uppercase">Specs</h3>
+            </div>
+        </div>
+
+        <div class="row flex">
+            <div class="col-2">
+                <h5>Art by:</h5>
+            </div>
+            <div class="col-4">
+                <p> @foreach ($cards['artists'] as $artist)
+                    <a href="#">{{ $artist }}</a>
+                    @endforeach
+                </p>
+            </div>
+            <div class="col-2">
+                <p>Series <br> <br> US.Price:</p>
+            </div>
+           
+            <div class="col-4">
+             <p class="uppercase">{{$cards["series"]}} <br> <br> {{$cards["price"]}}</p>
+            </div>
+        </div>
+        <div class="row flex">
+            <div class="col-2">
+                <h5>Written by:</h5>
+            </div>
+
+            <div class="col-4">
+                <p>@foreach ($cards['writers'] as $writer)
+                    <a href="#">{{ $writer }}</a>
+                    @endforeach
+                </p>
+            </div>
+            <div class="col-2">
+                <p>On Sale Date: </p>
+            </div>
+            <div class="col-2">
+                <p>{{$cards["sale_date"]}}</p>
+
+            </div>
+        </div>
 
 
-    <h5>Art by:</h5>
-    <p> @foreach ($cards['artists'] as $artist)
-        <a href="#">{{ $artist }}</a>
-        @endforeach
-    </p>
 
 
-    <h5>Written by:</h5>
-    <p>@foreach ($cards['writers'] as $writer)
-        <a href="#">{{ $writer }}</a>
-        @endforeach
-    </p>
 
-    <p>{{$cards["series"]}}</p>
-    <p>{{$cards["price"]}}</p>
-    <p>{{$cards["sale_date"]}}</p>
+    </section>
+
+
 </div>
 
 
